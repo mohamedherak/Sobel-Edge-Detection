c14f8b71cd4d60fe5546053d3ba8067f5c5ebb69
Mohamed Herak <32752875+mohamedherak@users.noreply.github.com>
2022-03-09 15:29:03 +0100

    Update sobel.py

--- sobel.py
+++ sobel.py
@@ -1,7 +1,7 @@
 from skimage.filters import sobel
 import cv2
 
-img = cv2.imread('shoreline.jpg', 0)
+img = cv2.imread('image_name', 0)
 sobel_img = sobel(img)
 cv2.imshow("Original", img)
 cv2.imshow("Sobel", sobel_img)
